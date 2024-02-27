8f8cc3be2724452b8ffb7e4615b2f47f14e5a945
Kevin Smith <kevin@muny.us>
2024-02-27 09:42:13 -0500

    Continue implementation

--- js/index.js
+++ js/index.js
@@ -80,8 +80,27 @@ class SerialDevice extends HTMLElement
     model = null;
     name = null;
     serialNumber = null;
-    vslider = null;
+
+    vsetSlider = null;
     vsetValue = null;
+
+    isetSlider = null;
+    isetValue = null;
+
+    outModeCVIndicator = null;
+    outModeCCIndicator = null;
+    outModeValue = null;
+
+    stateRelayIndicator = null;
+    stateOutputIndicator = null;
+
+    vout = 0;
+    iout = 0;
+    pout = 0;
+    tout = 0;
+    outMode = 'CV';
+    stateRelay = false;
+    stateOutput = false;
 
     constructor(port)
     {
@@ -103,10 +122,6 @@ class SerialDevice extends HTMLElement
 
             this.readSerial();
 
-            this.writeLine('id');
-
-            globalThis.write = this.writeLine.bind(this);
-
             setTimeout(() =>
             {
                 if (this.classList.contains('discovering'))
@@ -122,6 +137,10 @@ class SerialDevice extends HTMLElement
                     }, 4000);
                 }
             }, 4000);
+
+            await this.writeLine('id');
+
+            globalThis.write = this.writeLine.bind(this);
         }
         catch (error)
         {
@@ -179,7 +198,7 @@ class SerialDevice extends HTMLElement
 
                         line = lineParts.pop();
 
-                        lineParts.forEach(linePart =>
+                        lineParts.forEach(async linePart =>
                         {
                             console.log(linePart);
 
@@ -208,58 +227,190 @@ class SerialDevice extends HTMLElement
 
                                 this.replaceChildren(clone);
 
-                                this.vslider = this.querySelector('.vslider');
+                                this.vsetSlider = this.querySelector('.vset-slider');
                                 this.vsetValue = this.querySelector('.vset-value');
 
-                                this.vslider.addEventListener('input', e => {
-                                    this.writeLine(`vset,${e.target.value}`);
+                                this.vsetSlider.addEventListener('input', async e => {
+                                    await this.writeLine(`vset,${e.target.value}`);
                                 });
 
-                                this.writeLine('vset');
+                                await this.writeLine('vset');
 
+                                this.isetSlider = this.querySelector('.iset-slider');
+                                this.isetValue = this.querySelector('.iset-value');
 
-                            }
-                            else if (linePart.indexOf('vset,') === 0)
-                            {
-                                const vsetParts = linePart.split(',');
+                                this.isetSlider.addEventListener('input', async e => {
+                                    await this.writeLine(`iset,${e.target.value}`);
+                                });
 
-                                this.vslider.value = vsetParts[1];
+                                await this.writeLine('iset');
 
-                                const newValue = Number( (this.vslider.value - this.vslider.min) * 100 / (this.vslider.max - this.vslider.min) );
-                                const newPosition = 12 - (newValue * 0.39);
-                                this.vsetValue.innerHTML = `<span>${Number(this.vslider.value).toFixed(2)} V</span>`;
-                                this.vsetValue.style.bottom = `calc(${newValue}% + (${newPosition}px))`;
-                            }
-                            else if (linePart.indexOf('temp,') === 0)
-                            {
-                                const tempParts = linePart.trim().split(',');
+                                this.outModeCVIndicator = this.querySelector('.vout-cv-mode');
+                                this.outModeCCIndicator = this.querySelector('.iout-cc-mode');
+                                this.outModeValue = this.querySelector('.out-mode-value');
 
-                                const measurements = this.querySelector('.measurements-5-ch');
-
-                                if (measurements)
+                                this.stateRelayIndicator = this.querySelector('.relay-state');
+                                this.stateRelayIndicator.addEventListener('click', async e =>
                                 {
-                                    for (let i = 0; i < 5; i++)
+                                    await this.writeLine(`relay,${this.stateRelay ? 'off' : 'on'}`);
+                                });
+
+                                await this.writeLine('relay');
+                                this.stateOutputIndicator = this.querySelector('.output-state');
+                                this.stateOutputIndicator.addEventListener('click', async e =>
+                                {
+                                    await this.writeLine(`output,${this.stateOutput ? 'off' : 'on'}`);
+                                });
+
+                                await this.writeLine('output');
+
+                                const statusInterval = setInterval(async () =>
+                                {
+                                    if (!this.port.writable)
                                     {
-                                        const measurement = measurements.children[i];
-
-                                        if (measurement)
-                                        {
-                                            let val = Number(tempParts[i + 2]);
-                                            
-                                            // round to 1 decimal place
-                                            if (Math.abs(val) >= 100)
-                                                val = val.toFixed(1);
-                                            else
-                                                val = val.toFixed(2);
-
-                                            if (tempParts[i + 2] == '')
-                                                val = ' ';
-
-                                            measurement.querySelector('.measurement-value').innerText = val;
-                                        }
+                                        clearInterval(statusInterval);
+                                        return;
                                     }
+
+                                    await this.writeLine('status');
+                                }, 100);
+                            }
+                            else if (!this.classList.contains('discovering'))
+                            {
+                                if (linePart.indexOf('vset,') === 0)
+                                {
+                                    const vsetParts = linePart.split(',');
+
+                                    this.vsetSlider.value = vsetParts[1];
+
+                                    const newValue = Number( (this.vsetSlider.value - this.vsetSlider.min) * 100 / (this.vsetSlider.max - this.vsetSlider.min) );
+                                    const newPosition = 16 - (newValue * 0.43);
+                                    this.vsetValue.innerHTML = `<span>${Number(this.vsetSlider.value).toFixed(2)} V</span>`;
+                                    this.vsetValue.style.bottom = `calc(${newValue}% + (${newPosition}px))`;
+                                }
+                                else if (linePart.indexOf('iset,') === 0)
+                                {
+                                    const isetParts = linePart.split(',');
+
+                                    this.isetSlider.value = isetParts[1];
+
+                                    const newValue = Number( (this.isetSlider.value - this.isetSlider.min) * 100 / (this.isetSlider.max - this.isetSlider.min) );
+                                    const newPosition = 3 - (newValue * 0.305);
+                                    this.isetValue.innerHTML = `<span>${Number(this.isetSlider.value).toFixed(3)} A</span>`;
+                                    this.isetValue.style.bottom = `calc(${newValue}% + (${newPosition}px)`;
+                                }
+                                else if (linePart.indexOf('status,') === 0)
+                                {
+                                    const statusParts = linePart.split(',');
+                                    const statusScope = statusParts[1];
+                                    const statusParam = statusParts[2];
+                                    const statusValue = statusParts[3];
+
+                                    switch(statusScope)
+                                    {
+                                        case 'ina228':
+                                        {
+                                            switch(statusParam)
+                                            {
+                                                case 'vout':
+                                                {
+                                                    let vout = Number(statusValue);
+                                                    if (vout < 0)
+                                                        vout = 0;
+
+                                                    this.vout = vout;
+
+                                                    this.querySelector('.vout-value').innerText = vout.toFixed(2);
+                                                    this.updatePowerLabel();
+                                                }
+                                                break;
+
+                                                case 'iout':
+                                                {
+                                                    let iout = Number(statusValue);
+                                                    if (iout < 0)
+                                                        iout = 0;
+
+                                                    this.iout = iout;
+
+                                                    this.querySelector('.iout-value').innerText = iout.toFixed(3);
+                                                    this.updatePowerLabel();
+                                                }
+                                                break;
+                                            }
+                                        }
+                                        break;
+
+                                        case 'tmp100':
+                                        {
+                                            switch(statusParam)
+                                            {
+                                                case 'temp':
+                                                {
+                                                    let temp = Number(statusValue);
+
+                                                    this.tout = temp;
+
+                                                    this.querySelector('.tout-value').innerText = temp.toFixed(1);
+                                                
+                                                }
+                                                break;
+                                            }
+                                        }
+
+                                        case 'tps55289':
+                                        {
+                                            switch(statusParam)
+                                            {
+                                                // short circuit protection
+                                                case 'scp':
+                                                {
+                                                    
+                                                }
+                                                break;
+                                                
+                                                // over current protection
+                                                case 'ocp':
+                                                {
+                                                    if (statusValue == 'true')
+                                                    {
+                                                        this.outMode = 'CC';
+                                                        this.outModeCVIndicator.style.opacity = 0;
+                                                        this.outModeCCIndicator.style.opacity = 1;
+                                                    }
+                                                    else
+                                                    {
+                                                        this.outMode = 'CV';
+                                                        this.outModeCVIndicator.style.opacity = 1;
+                                                        this.outModeCCIndicator.style.opacity = 0;
+                                                    }
+
+                                                    this.outModeValue.innerText = this.outMode;
+                                                }
+                                                break;
+                                            }
+
+                                        }
+                                        break;
+                                    }
+
+                                }
+                                else if (linePart.indexOf('relay,') === 0)
+                                {
+                                    const relayParts = linePart.split(',');
+                                    this.stateRelay = relayParts[1] === 'on';
+                                    this.stateRelayIndicator.classList.toggle('active', this.stateRelay);
+                                    this.stateRelayIndicator.querySelector('.state-value').innerText = this.stateRelay ? 'ON' : 'OFF';
+                                }
+                                else if (linePart.indexOf('output,') === 0)
+                                {
+                                    const outputParts = linePart.split(',');
+                                    this.stateOutput = outputParts[1] === 'on';
+                                    this.stateOutputIndicator.classList.toggle('active', this.stateOutput);
+                                    this.stateOutputIndicator.querySelector('.state-value').innerText = this.stateOutput ? 'ON' : 'OFF';
                                 }
                             }
+                            
                         });
                     }
                 }
@@ -290,6 +441,11 @@ class SerialDevice extends HTMLElement
     attributeChangedCallback(name, oldValue, newValue)
     {
         console.log(`Attribute: ${name} changed from ${oldValue} to ${newValue}`);
+    }
+
+    updatePowerLabel()
+    {
+        this.querySelector('.pout-value').innerText = (this.vout * this.iout).toFixed(2);
     }
 }
 
